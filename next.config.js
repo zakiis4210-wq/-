fdb7bbd04fdf53c7be2a22b31f784b751478ca10
zakiis4210-wq <zaki.is4210@gmail.com>
2026-01-30 05:43:17 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  output: "export",
+
+  // GitHub Pages 用（← あなたのリポジトリ名）
+  basePath: "/zakiis4210-wq",
+  assetPrefix: "/zakiis4210-wq/",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
