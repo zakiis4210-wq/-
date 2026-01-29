fb087d5d565b69a55a8b5c470ccbdce4b3239ab7
zakiis4210-wq <zaki.is4210@gmail.com>
2026-01-30 05:47:50 +0900

    Update page.tsx

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -12,26 +12,26 @@ https://nextjs.org/telemetry
   ▲ Next.js 14.2.5
 
    Creating an optimized production build ...
- ✓ Compiled successfully
-   Linting and checking validity of types ...
+Failed to compile.
 
-   We detected TypeScript in your project and reconfigured your tsconfig.json file for you. Strict-mode is set to false by default.
-   The following suggested values were added to your tsconfig.json. These values can be changed to fit your project's needs:
+./app/recipes/[id]/page.tsx
+Error: 
+  x Expected ';', '}' or <eof>
+   ,-[/home/runner/work/-/-/app/recipes/[id]/page.tsx:1:1]
+ 1 | Run npm run build
+   : ^|^ ^^^
+   :  `-- This is the expression part of an expression statement
+ 2 | 
+ 3 | > build
+ 4 | > next build
+   `----
 
-   	- include was updated to add '.next/types/**/*.ts'
-   	- plugins was updated to add { name: 'next' }
+Caused by:
+    Syntax Error
 
-   Collecting page data ...
+Import trace for requested module:
+./app/recipes/[id]/page.tsx
 
-> Build error occurred
-Error: Page "/recipes/[id]" is missing "generateStaticParams()" so it cannot be used with "output: export" config.
-    at /home/runner/work/-/-/node_modules/next/dist/build/index.js:1294:59
-    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
-    at async Span.traceAsyncFn (/home/runner/work/-/-/node_modules/next/dist/trace/trace.js:154:20)
-    at async Promise.all (index 7)
-    at async /home/runner/work/-/-/node_modules/next/dist/build/index.js:1172:17
-    at async Span.traceAsyncFn (/home/runner/work/-/-/node_modules/next/dist/trace/trace.js:154:20)
-    at async /home/runner/work/-/-/node_modules/next/dist/build/index.js:1095:124
-    at async Span.traceAsyncFn (/home/runner/work/-/-/node_modules/next/dist/trace/trace.js:154:20)
-    at async build (/home/runner/work/-/-/node_modules/next/dist/build/index.js:366:9)
+
+> Build failed because of webpack errors
 Error: Process completed with exit code 1.
